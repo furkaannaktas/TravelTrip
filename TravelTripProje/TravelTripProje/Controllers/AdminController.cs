900b7ee0c6b8228f034a2ca0cec5c98aca073dbe
furkaannaktas <furkaannaktas05@gmail.com>
2025-01-20 21:57:28 +0300

    TravelTrip
    MVC mimarisi kullanılmıştır.

---
+++ TravelTripProje/TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]  
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil(int id)
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir(int id)
+        {
+            var bl = c.Blogs.Find(id);
+            return View("BlogGetir", bl);
+        }
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blg = c.Blogs.Find(b.ID);
+            if (blg != null)
+            {
+                blg.Aciklama = b.Aciklama;
+                blg.Baslik = b.Baslik;
+                blg.BlogImage = b.BlogImage;
+
+                // Tarih kontrolü
+                if (b.Tarih == null || b.Tarih < new DateTime(1753, 1, 1))
+                {
+                    blg.Tarih = DateTime.Now; // Varsayılan tarih olarak bugünü kullan
+                }
+                else
+                {
+                    blg.Tarih = b.Tarih;
+                }
+
+                c.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+         public ActionResult YorumListesi()
+        {
+           var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        public ActionResult YorumGetir(int id)
+        {
+            var yr = c.Yorumlars.Find(id);
+            return View("YorumGetir", yr);
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yrm = c.Yorumlars.Find(y.ID);
+
+            if (yrm != null)
+            {
+                // Yorumları, kullanıcı adı, mail, yorum alanlarını yeni değerlerle güncelle
+                yrm.KullaniciAdi = y.KullaniciAdi;
+                yrm.Mail = y.Mail;
+                yrm.Yorum = y.Yorum;
+
+                // Veritabanındaki değişiklikleri kaydet
+                c.SaveChanges();
+            }
+
+            // Güncelleme işlemi başarılı olduğunda YorumListesi sayfasına yönlendir
+            return RedirectToAction("YorumListesi");
+        }
+
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Controllers/IletisimController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // Veritabanı bağlamı (Context)
+        Context c = new Context();
+
+        // GET: İletisim
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // İletişim formunu gönderen metot
+        [HttpPost]
+        public ActionResult MesajGonder(string AdSoyad, string Konu, string Mesaj)  // AdSoyad, Konu ve Mesaj parametre olarak alınır
+        {
+            if (!string.IsNullOrEmpty(AdSoyad) && !string.IsNullOrEmpty(Konu) && !string.IsNullOrEmpty(Mesaj))  // Formun tüm alanları dolu olmalı
+            {
+                // Yeni bir iletisim nesnesi oluşturuluyor
+                iletisim yeniMesaj = new iletisim()
+                {
+                    AdSoyad = AdSoyad,
+                    Konu = Konu,
+                    Mesaj = Mesaj
+                };
+
+                // Veritabanına ekleme işlemi
+                c.iletisims.Add(yeniMesaj);  // Yeni mesaj veritabanına eklenir
+                c.SaveChanges();  // Veritabanına kaydedilir
+
+                // Kullanıcıya teşekkür mesajı
+                ViewBag.Message = "Mesajınız başarıyla gönderildi. Teşekkür ederiz!";
+                return View("Index");
+            }
+
+            // Eğer formda herhangi bir alan boş ise, form tekrar gösterilir.
+            ViewBag.Message = "Lütfen tüm alanları doldurduğunuzdan emin olun.";
+            return View("Index");
+        }
+    }
+}
